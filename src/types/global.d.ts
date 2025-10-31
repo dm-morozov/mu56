66ba1affe0e04e6e1cce4b161175e561dccadf6d
dm-morozov <dem.morozov@gmail.com>
2025-10-31 20:44:39 +0300

    feat: Продвигаем сайт по SEO 002

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,9 @@
 // src/types/global.d.ts
 interface Window {
-  ym?: (counterId: number, method: string, event: string, params?: any) => void
+  ym?: (
+    counterId: number,
+    method: 'init' | 'reachGoal' | 'hit' | string,
+    event?: string,
+    params?: Record<string, unknown>
+  ) => void
 }
